3b97921dea22a6e5a22931e38c0f29d5115e90b2
wuxingli3426 <wuxingli3426@ipalfish.com>
2020-08-25 21:32:24 +0800

    fix: 修改bug

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,14 +9,43 @@ module.exports = {
   //   siteTitleAlt: `Emilia - Gatsby Starter Portfolio`,
   // },
   siteMetadata: {
-    title: 'xinglee23',
-    siteUrl: 'https://emilia.lekoarts.de'
+    // Used for the title template on pages other than the index site
+    siteTitle: `xinglee23`,
+    // Default title of the page
+    siteTitleAlt: `Emilia - @lekoarts/gatsby-theme-emilia`,
+    // Can be used for e.g. JSONLD
+    siteHeadline: `Emilia - Gatsby Theme from @lekoarts`,
+    // Will be used to generate absolute URLs for og:image etc.
+    siteUrl: `https://emilia.lekoarts.de`,
+    // Used for SEO
+    siteDescription: `Minimalistic portfolio/photography site with masonry grid, page transitions and big images. Themeable with Theme UI. Includes Light/Dark mode.`,
+    // Will be set on the <html /> tag
+    siteLanguage: `en`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
+    // Twitter Handle
+    author: `@lekoarts_de`,
   },
   plugins: [
     {
       resolve: `@lekoarts/gatsby-theme-emilia`,
       // See the theme's README for all available options
-      options: {},
+      options: {
+        name: `xinglee23`,
+        location: `Beijing China`,
+        socialMedia: [
+          {
+            title: `facebook`,
+            href: `https://www.facebook.com/xingli.wu.10`,
+          },
+          {
+            title: `twitter`,
+            href: `https://twitter.com/Tracyxing`,
+          },
+        ],
+        showThemeAuthor: true,
+        formatString: `YYYY.MM.DD`,
+      },
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
